94293945227b0c7a70c80b96044446bcdeade7b7
stefanoreddd <stefanogg95@gmail.com>
2023-04-27 15:56:54 +0200

    class products fix.

---
+++ C# Shop 3/Products.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Shop_3
+{
+    public class Products
+    {
+
+        private static int numberOfGeneratedProducts = 0;
+
+        //ATTRIBUTI
+
+        private string productName;
+        private string productCategory;
+        private string productDescription;
+        private int productPrice;
+        
+
+
+        //COSTRUTTORE
+
+        public Products(string productName, string productCategory, string productDescription, int productPrice)
+        {
+            this.productName = productName;
+            this.productCategory = productCategory;
+            this.productDescription = productDescription;
+            this.productPrice = productPrice;
+
+        }
+
+
+        //GETTERS
+
+        public string GetProductName()
+        {
+            return this.productName;
+        }
+
+        public string GetProductCategory()
+        {
+            return this.productCategory;
+        }
+
+        public string GetProductDescription()
+        {
+            return this.productDescription;
+        }
+
+        public int GetProductPrice()
+        {
+            return this.productPrice;
+        }
+
+
+        //SETTERS
+
+
+        public void SetProductName(string productName)
+        {
+            this.productName = productName;
+        }
+
+        public void SetProductCategory(string category)
+        {
+            this.productCategory = category;
+        }
+
+        public void SetProductDescription(string description)
+        {
+            this.productDescription = description;
+        }
+
+
+
+        //METODI
+
+        public string ProductInfoList()
+        {
+            string productInfo = "Nome Prodotto: " + this.productName + "\n";
+            productInfo += "Categoria: " + this.productCategory + "\n";
+            productInfo += "Descrizione: " + this.productDescription + "\n";
+            productInfo += "Prezzo: " + this.productPrice + " euro" + "\n";
+
+
+            return productInfo;
+
+        }
+    }
+}
